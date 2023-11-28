a31fb694ae19d83b3b565bb50481263fc13bbe5f
AJINTHA S <ajinthasivam2019@gmail.com>
2023-11-28 14:57:34 +0530

    Add files via upload

---
+++ movies/src/main/java/com/example/movies/ReviewService.java
@@ -0,0 +1,26 @@
+package com.example.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    ReviewRepository reviewRepository;
+    @Autowired
+    MongoTemplate template;
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+
+        template.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
